1b48655da087fe4a940f592f6583e9268b98ff9e
Norberto Alonso <alonsonorberto@gmail.com>
2021-08-01 17:07:59 +0100

    wip

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,12 @@ function showFormConfirmation(){
     var element = document.querySelector(".cyn-confirmation-form");
     element.classList.add("showBlock");
 }
+
+function scrollDown(){
+    if (window.scrollY > 0) {
+        document.body.classList.add("mod--scroll-down");
+    }else {
+        document.body.classList.remove("mod--scroll-down");
+    }
+}
+
